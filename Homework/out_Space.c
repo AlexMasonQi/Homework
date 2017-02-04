0e9865502a128fca1095a42642c4a764ca289b5c
徐世琦 <xushiqidell@gmail.com>
2017-02-04 18:16:13 +0800

    添加项目文件。

---
+++ Homework/out_Space.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int trimSpace(char *inbuf, char *outbuf)
+{
+	int ret = 0;
+	if (inbuf == NULL)
+	{
+		ret = -1;
+		printf("The func trimSpace() err:%d\n", ret);
+		exit(0);
+	}
+}
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
